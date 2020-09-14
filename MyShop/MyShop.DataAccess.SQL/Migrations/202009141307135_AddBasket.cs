b8771616c645bf3a9b01805637f0d4ada5837c02
Shubham sharma <shubhbhardwaj12@gmail.com>
2020-09-14 18:38:44 +0530

    BasketModel Created

---
+++ MyShop/MyShop.DataAccess.SQL/Migrations/202009141307135_AddBasket.cs
@@ -0,0 +1,45 @@
+namespace MyShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBasket : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BasketItems",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        BasketId = c.String(maxLength: 128),
+                        ProductId = c.String(),
+                        Quantity_HelpLink = c.String(),
+                        Quantity_Source = c.String(),
+                        Quantity_HResult = c.Int(nullable: false),
+                        CreateDate = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Baskets", t => t.BasketId)
+                .Index(t => t.BasketId);
+            
+            CreateTable(
+                "dbo.Baskets",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        CreateDate = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.BasketItems", "BasketId", "dbo.Baskets");
+            DropIndex("dbo.BasketItems", new[] { "BasketId" });
+            DropTable("dbo.Baskets");
+            DropTable("dbo.BasketItems");
+        }
+    }
+}
